c3dd41e6595ab1fee0882913c212166dfa5c6649
jusdt-io <admin@jsonusdt.com>
2025-06-14 23:37:30 +0400

    Final reorganization cleanup

---
+++ contracts/JUSDC.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract JUSDC is ERC20, Ownable {
+    uint8 private constant _decimals = 6;
+
+    constructor(address initialOwner) ERC20("JUSDC", "JUSDC") Ownable(initialOwner) {}
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
